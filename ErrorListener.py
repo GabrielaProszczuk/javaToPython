09ef22147cdf33451dedae978e5b7f384ab9745c
KarolinaFilipiuk <k.filipiuk18@gmail.com>
2021-06-15 21:53:32 +0200

    correct error messages

--- ErrorListener.py
+++ ErrorListener.py
@@ -10,15 +10,5 @@ class ErrorListener( ErrorListener ):
         super(ErrorListener, self).__init__()
 
     def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
-        print("You gave a file with mistake, try again!"
-        "        Info: " + str(msg) + "  Line: " + str(line) + " Column: " + str(column))
-        #raise Exception("Oh no!!")
-
-    # def reportAmbiguity(self, recognizer, dfa, startIndex, stopIndex, exact, ambigAlts, configs):
-    #     raise Exception("Oh no!!")
-
-    # def reportAttemptingFullContext(self, recognizer, dfa, startIndex, stopIndex, conflictingAlts, configs):
-    #     raise Exception("Oh no!!")
-
-    # def reportContextSensitivity(self, recognizer, dfa, startIndex, stopIndex, prediction, configs):
-    #     raise Exception("Oh no!!")
+        print("You gave a file with syntax mistake, try again!"
+        "        Line: " + str(line) + " Column: " + str(column))
---
+++ example_error_output.py
@@ -0,0 +1,3 @@
+if (x<5):
+    while (2!=3):
+    s = "jestFajnie"
